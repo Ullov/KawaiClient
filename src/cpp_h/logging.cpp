ae003527dcf1e6f24689b432ae3d59f48e189b80
ullov <dorogobed.sasha@gmail.com>
2020-03-15 18:46:22 +0300

    Fix in Logging class
    Added flag Append.

--- src/cpp_h/logging.cpp
+++ src/cpp_h/logging.cpp
@@ -31,7 +31,7 @@ void Logging::writeCustomLog(const QString &message, const QString &from, const 
 {
     QString dateTime = QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss:zzz");
     QString dt = '[' + logTypeNames[type] + "][" + from + ']' + dateTime + ' ' + message + '\n';
-    NativeFs::writeFile(dt.toUtf8(), path, fileName);
+    NativeFs::writeFile(dt.toUtf8(), path, fileName, QIODevice::Append | QIODevice::WriteOnly);
 }
 
 void Logging::writeInfo(const QString &message, const QString &from)
