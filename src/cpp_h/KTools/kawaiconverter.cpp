af5aeb50a0514a5675667e6786b5e53fae3e7b67
ullov <dorogobed.sasha@gmail.com>
2020-04-14 17:28:54 +0300

    Fixed KawaiConverter
    Fixed conversion to QByteArray.

--- src/cpp_h/KTools/kawaiconverter.cpp
+++ src/cpp_h/KTools/kawaiconverter.cpp
@@ -5,22 +5,10 @@ KawaiConverter::KawaiConverter() {}
 template<typename T>
 T KawaiConverter::byteArrayToT(const QByteArray &bArr)
 {
-    if (typeid(T) == typeid(qint32))        // int[32]
-        return bArr.toInt();
-    else if (typeid(T) == typeid(quint32))  // uint[32]
-        return bArr.toUInt();
-
-    else if (typeid(T) == typeid(qint64))   // int[64]
-        return bArr.toLong();
-    else if (typeid(T) == typeid(quint64))  // uint[64]
-        return bArr.toULong();
-
-    else if (typeid(T) == typeid(qint16))   // int[16]
-        return bArr.toShort();
-    else if (typeid(T) == typeid(quint16))  // uint[16]
-        return bArr.toUShort();
-
-    return T();
+    QDataStream str(bArr);
+    T result;
+    str >> result;
+    return result;
 }
 template<>
 const char* KawaiConverter::byteArrayToT<const char*>(const QByteArray &bArr)
@@ -240,3 +228,12 @@ template qint32 KawaiConverter::byteArrayToT<qint32>(const QByteArray&);
 template quint32 KawaiConverter::byteArrayToT<quint32>(const QByteArray&);
 template qint64 KawaiConverter::byteArrayToT<qint64>(const QByteArray&);
 template quint64 KawaiConverter::byteArrayToT<quint64>(const QByteArray&);
+
+template QByteArray KawaiConverter::toByteArray<qint8>(const qint8&);
+template QByteArray KawaiConverter::toByteArray<quint8>(const quint8&);
+template QByteArray KawaiConverter::toByteArray<qint16>(const qint16&);
+template QByteArray KawaiConverter::toByteArray<quint16>(const quint16&);
+template QByteArray KawaiConverter::toByteArray<qint32>(const qint32&);
+template QByteArray KawaiConverter::toByteArray<quint32>(const quint32&);
+template QByteArray KawaiConverter::toByteArray<qint64>(const qint64&);
+template QByteArray KawaiConverter::toByteArray<quint64>(const quint64&);
