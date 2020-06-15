44d8b90dde8716395b11d312d00682b40d6698ff
ullov <dorogobed.sasha@gmail.com>
2020-06-15 04:19:32 +0300

    Function fix

--- src/js/Tools.js
+++ src/js/Tools.js
@@ -1,7 +1,7 @@
 function bytesToHumanReadable(bytes, mode)
 {
     var ext = "/s"
-    if (mode === null)
+    if (mode !== null)
         ext = ""
     if (bytes === 0)
         return 0 + " Bit" + ext
